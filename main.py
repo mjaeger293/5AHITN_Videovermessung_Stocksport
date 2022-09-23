dd8700d33276471cbbb88ff2669f3634d9b4c91d
frahofer <frahofer@htl-steyr.ac.at>
2022-09-23 10:49:53 +0200

    Draw line between circles and display difference in px

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 import numpy as np
 import argparse
 import cv2
+import math
+
+daubeIndex = 0
+smallestRadius = 0
+indexToDistance = {}
 
 # load the image, clone it for output, and then convert it to grayscale
 image = cv2.imread("kreise_gimp_verarbeitet_kontrast_helligkeit.png")
@@ -16,15 +21,39 @@ if circles is not None:
 	# convert the (x, y) coordinates and radius of the circles to integers
 	circles = np.round(circles[0, :]).astype("int")
 	# loop over the (x, y) coordinates and radius of the circles
+	i = 0
+	for (x, y, r) in circles:
+		if smallestRadius == 0 or r < smallestRadius:
+			smallestRadius = r
+			daubeIndex = i
+		i = i + 1
+
+	i = 0
 	for (x, y, r) in circles:
 		print(x,y,r)
 		# draw the circle in the output image, then draw a rectangle
 		# corresponding to the center of the circle
+
 		cv2.circle(output, (x, y), r, (0, 255, 0), 2)
 		cv2.rectangle(output, (x - 2, y - 2), (x + 2, y + 2), (0, 128, 255), -1)
+
+		if i != daubeIndex:
+			a = abs(circles[daubeIndex][0] - x)
+			b = abs(circles[daubeIndex][1] - y)
+
+			c = math.sqrt(a ** 2 + b ** 2)
+			#c = c - r - circles[daubeIndex][2]
+
+			indexToDistance[i] = c
+
+			cv2.putText(output, str(c), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0, 0, 0), 1)
+			cv2.line(output, (circles[daubeIndex][0], circles[daubeIndex][1]), (x, y), [0, 255, 0], 2)
+
+		i = i + 1
 	# show the output image
 	output = cv2.resize(output, (500,500))
 	cv2.imshow("output", output) #np.hstack([image, output])
 	cv2.waitKey()
+	print(circles[daubeIndex])
 else:
 	print("No circles found")
