26bf86c8d905d02958109542afcf4b0e4a8b6b3d
fsengsei <fsengsei@htl-steyr.ac.at>
2022-11-11 16:52:00 +0100

    Erkenntnis des Tages --> Wir brauchen neue BILDER

--- images/camera_calibration/calibrate.py
+++ images/camera_calibration/calibrate.py
@@ -5,42 +5,40 @@ import glob
 import os
 from os import listdir
 
-
 CHECKERBOARD = (4, 4)
-subpix_criteria = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
-calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHECK_COND+cv2.fisheye.CALIB_FIX_SKEW
-objp = np.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
-objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+subpix_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC + cv2.fisheye.CALIB_CHECK_COND + cv2.fisheye.CALIB_FIX_SKEW
+objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+objp[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 _img_shape = None
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
 
 images = []
 
-folder_dir = "D:\\Schule\\5AHITN\Schweiger\\5AHITN_Videovermessung_Stocksport\\images\\camera_calibration"
+folder_dir = r"C:\Users\Daisy\OneDrive - HTBLA Steyr\Htl - Steyr\Inegneursprojekt\project\5AHITN_Videovermessung_Stocksport\images\camera_calibration"
 for img in os.listdir(folder_dir):
-    if (img.endswith("__2022-11-04__15-38-25.jpg")):
+    if (img.startswith("Image")):
         images.append(img)
 
-#images.append(cv2.imread('D:\\Schule\\5AHITN\Schweiger\\5AHITN_Videovermessung_Stocksport\\images\\camera_calibration\\Image__2022-11-04__15-38-25.jpg'))
+# images.append(cv2.imread('D:\\Schule\\5AHITN\Schweiger\\5AHITN_Videovermessung_Stocksport\\images\\camera_calibration\\Image__2022-11-04__15-38-25.jpg'))
 print(len(images))
 i = 0
 
 for fname in images:
     img = cv2.imread(fname)
     print(f"{i}: {fname}")
-    if _img_shape == None:
-        _img_shape = img.shape[:2]
-    else:
-        assert _img_shape == img.shape[:2], "All images must share the same size."
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    _img_shape = img.shape[:2]
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
-    #ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
-    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, flags=cv2.CALIB_CB_FAST_CHECK)
-    # If found, add object points, image points (after refining them)
-    if ret == True:
+    # ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD,
+                                        calibration_flags)    # If found, add object points, image points (after refining them)
+    if ret:
+        print(i)
         objpoints.append(objp)
-        cv2.cornerSubPix(gray,corners,(3,3),(-1,-1),subpix_criteria)
+        cv2.cornerSubPix(gray, corners, (3, 3), (-1, -1), subpix_criteria)
         imgpoints.append(corners)
     i += 1
 N_OK = len(objpoints)
@@ -58,7 +56,7 @@ rms, _, _, _, _ = \
         rvecs,
         tvecs,
         calibration_flags,
-        (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
+        (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
     )
 print("Found " + str(N_OK) + " valid images for calibration")
 print("DIM=" + str(_img_shape[::-1]))
