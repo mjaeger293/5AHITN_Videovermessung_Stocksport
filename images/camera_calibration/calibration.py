171811db14bfed9620ff2d244c643a9c4c704543
frahofer <frahofer@htl-steyr.ac.at>
2022-11-25 09:20:31 +0100

    images get saved to /undistorted

--- images/camera_calibration/calibration.py
+++ images/camera_calibration/calibration.py
@@ -101,7 +101,10 @@ def undistort(img_path):
     h,w = img.shape[:2]
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-    viewImage(undistorted_img)
+    #viewImage(undistorted_img)
+    #print(img_path.split("\\").pop())
+    cv2.imwrite("../undistorted/" + img_path.split("\\").pop(), undistorted_img)
+    print("done")
 
 
 images.clear()
